f08a6863b6c31674b005c3a9e022b3731c656ea0
Karl-Johan Alm <karljohan-alm@garage.co.jp>
2017-07-25 12:44:31 +0900

    Added fixed seeds for BC2.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -331,7 +331,7 @@ public:
         base58Prefixes[EXT_PUBLIC_KEY] = {0x04, 0x88, 0xB2, 0x1E};
         base58Prefixes[EXT_SECRET_KEY] = {0x04, 0x88, 0xAD, 0xE4};
 
-        vFixedSeeds.clear(); // = std::vector<SeedSpec6>(pnSeed6_main, pnSeed6_main + ARRAYLEN(pnSeed6_main));
+        vFixedSeeds = std::vector<SeedSpec6>(pnSeed6_bc2, pnSeed6_bc2 + ARRAYLEN(pnSeed6_bc2));
         vSeeds.clear();
 
         fDefaultConsistencyChecks = false;
